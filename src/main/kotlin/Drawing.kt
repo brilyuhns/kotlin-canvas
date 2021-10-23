79b2ba16a5ca4e5b79da4d126ea374d454adeaa0
Divya <kunnathdivyal@gmail.com>
2021-10-23 22:35:44 +0800

    Refactor canvasToString to a separate function

--- src/main/kotlin/Drawing.kt
+++ src/main/kotlin/Drawing.kt
@@ -1,15 +1,18 @@
 class Drawing {
     fun execute(command: String, length: Int, height: Int):String {
-        val x = if(command == "C"){
+        val canvas = if(command == "C"){
             emptyCanvas(length, height)
-        } else { "" }
-        return x
+        } else { arrayOf(arrayOf(" ")) }
+        return canvasToString(canvas, length)
     }
 
-    fun emptyCanvas(length: Int, height: Int): String {
-        val arr = Array(height) { Array(length){ " " } }
+    fun emptyCanvas(length: Int, height: Int): Array<Array<String>> {
+        return Array(height) { Array(length){ " " } }
+    }
+
+    fun canvasToString(canvas: Array<Array<String>>, length: Int): String {
         var x = horizontalBorderLine("-", length)
-        for(row in arr){
+        for(row in canvas){
             x = x.plus("|" ).plus(row.joinToString("")).plus( "|\n")
         }
         x = x.plus(horizontalBorderLine("-", length))
